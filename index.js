165e06366223ceacc95eebd65122921e51b60cd9
Sven Flickinger <hc4e2k@gmail.com>
2017-11-02 17:08:47 +0100

    Refactor removing coffeescript

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-require('coffee-script/register');
-require('./index.coffee')(hexo);
+const gzipme = require('gzipme');
+const walk = require('walk');
+const chalk = require('chalk');
+
+hexo.extend.console.register('gzip', gzipFn);
+
+function gzipFn (args, callback) {
+    const log = this.log;
+    const files = [];
+    const start = new Date();
+
+    log.info(`[${chalk.cyan('GZIP')}] Compressing files...`);
+    const walker = walk.walk('public', { followLinks: false });
+
+    walker.on('file', (root, stat, next) => {
+      const file = `${root}/${stat.name}`;
+      log.info(`[${chalk.cyan('GZIP')}] Compressing: ${chalk.magenta(file)}`);
+      gzipme(file, true, 'best');
+      next();
+    });
+
+    walker.on('end', () => {
+      const end = new Date();
+      log.info(`[${chalk.cyan('GZIP')}] Compressing finished after ${chalk.cyan(end - start)} ms`);
+    });
+}
