4e78581689d6ef80d9ef49db086d0db08211eb96
Sven Flickinger <hc4e2k@gmail.com>
2017-11-03 09:28:33 +0100

    Remove gzipme using zlib directly

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-const gzipme = require('gzipme');
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
 const walk = require('walk');
 const chalk = require('chalk');
 
 hexo.extend.console.register('gzip', gzipFn);
 
 function gzipFn (args, callback) {
-    const log = this.log;
-    const files = [];
-    const start = new Date();
+  const log = this.log;
+  const files = [];
+  const start = new Date();
 
-    log.info(`[${chalk.cyan('GZIP')}] Compressing files...`);
-    const walker = walk.walk('public', { followLinks: false });
+  log.info(`[${chalk.cyan('GZIP')}] Compressing files...`);
+  const walker = walk.walk('public', { followLinks: false });
 
-    walker.on('file', (root, stat, next) => {
-      const file = `${root}/${stat.name}`;
-      log.info(`[${chalk.cyan('GZIP')}] Compressing: ${chalk.magenta(file)}`);
-      gzipme(file, true, 'best');
-      next();
+  walker.on('file', (root, stat, next) => {
+    const file = `${root}/${stat.name}`;
+    const gzFile = `${file}.gz`;
+    const gzip = zlib.createGzip({
+      level: zlib.Z_BEST_COMPRESSION,
+      memLevel: zlib.Z_BEST_COMPRESSION
     });
 
-    walker.on('end', () => {
-      const end = new Date();
-      log.info(`[${chalk.cyan('GZIP')}] Compressing finished after ${chalk.cyan(end - start)} ms`);
-    });
+    log.info(`[${chalk.cyan('GZIP')}] Compressing: ${chalk.magenta(file)}`);
+
+    const input = fs.createReadStream(path.resolve(file));
+    const output = fs.createWriteStream(path.resolve(gzFile));
+    input.pipe(gzip).pipe(output);
+
+    output
+      .on('error', () => {
+        throw new Error(`Error gzipping file "${filePath}"`);
+      })
+      .on('close', () => {
+        fs.unlinkSync(file);
+        fs.renameSync(gzFile, file)
+      });
+    next();
+  });
+
+  walker.on('end', () => {
+    const end = new Date();
+    log.info(`[${chalk.cyan('GZIP')}] Compressing finished after ${chalk.cyan(end - start)} ms`);
+  });
 }
